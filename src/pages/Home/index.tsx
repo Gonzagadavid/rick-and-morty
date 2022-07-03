c95a8f2362c1270efb0085b55129e2947959b2e7
gonzagadavid <david.blesses@hotmail.com>
2022-07-03 19:19:58 -0300

    feat: add RouteCard in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
-import { Box, Typography } from '@mui/material';
-import { FC } from 'react';
+import { Box } from '@mui/material';
+import { RouteCard } from 'components/RouteCard';
+import { FC, useMemo } from 'react';
+import { RouteNames } from 'routes';
 
-const Home: FC = () => (
-  <Box>
-    <Typography>Home</Typography>
-  </Box>
-);
+const { HOME, ...routes } = RouteNames;
+
+const Home: FC = () => {
+  const routesList = useMemo(() => (
+    Object.entries(routes).map(([routeName, path]) => (
+      <RouteCard key={routeName} title={routeName} path={path} />
+    ))
+  ), []);
+
+  return (
+    <Box>
+      {routesList}
+    </Box>
+  );
+};
 
 export default Home;
