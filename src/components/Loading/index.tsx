112d6e48c1422c833b3488bc302be1c9eb3b4937
gonzagadavid <david.blesses@hotmail.com>
2022-07-03 16:10:06 -0300

    refactor: adjust Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,16 +1,9 @@
-import { useReactiveVar } from '@apollo/client';
 import { CircularProgress, Box } from '@mui/material';
-import { loadingState } from 'hooks/useLoading';
 import { FC } from 'react';
 import { styles } from './styles';
 
-export const Loading:FC = () => {
-  const isLoading = useReactiveVar(loadingState);
-  if (!isLoading) return null;
-
-  return (
-    <Box sx={styles.container}>
-      <CircularProgress />
-    </Box>
-  );
-};
+export const Loading:FC = () => (
+  <Box sx={styles.container}>
+    <CircularProgress />
+  </Box>
+);
