e284113bdb9caaaf7bab2570d3d3b24b50352640
gonzagadavid <david.blesses@hotmail.com>
2022-07-10 17:20:22 -0300

    feat: add episodes query

---
+++ src/services/graphql/getEpisodes.ts
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+import { CharacterType } from './getCharacters';
+
+export interface EpisodesType {
+  id: number;
+  name: string;
+  air_date: string;
+  created: string;
+  results: CharacterType[]
+}
+
+export const EPISODES = gql`
+ query Episodes ($page: Int, $filter: FilterEpisode) {
+    episodes(page: $page, filter: $filter) {
+      info {
+        next
+        prev
+        count
+        pages
+      }
+      results {
+        id
+        name
+        air_date
+        episode
+        created
+        characters {
+          ... on Character {
+         id
+         name
+         image
+          }
+        }
+      }
+    }
+  }
+`;
