e47bff19eaa1fee1cd2025ce87fa7e56461dc821
gonzagadavid <david.blesses@hotmail.com>
2022-07-24 17:47:51 -0300

    feat: adds Skeleton in Character page

--- src/constants/numbers.ts
+++ src/constants/numbers.ts
@@ -4,4 +4,8 @@ export const ONE = 1;
 
 export const TWO = 2;
 
+export const FIVE = 5;
+
+export const NINE = 9;
+
 export const TWENTY = 20;
---
+++ src/pages/Charater/SkeletonCharacter/index.tsx
@@ -0,0 +1,39 @@
+import {
+  Box, Paper, Skeleton, Chip,
+} from '@mui/material';
+import { FIVE, NINE, TWENTY } from 'constants/numbers';
+import { EMPTY } from 'constants/strings';
+import { useMemo } from 'react';
+import { styles } from './styles';
+
+export const SkeletonCharacter = () => {
+  const chips = useMemo(() => (
+    Array(NINE).fill(EMPTY).map((_, index) => (
+      <Skeleton key={`${index + TWENTY}`} sx={styles.chip}>
+        <Chip sx={styles.chip} />
+      </Skeleton>
+    ))
+  ), []);
+
+  const infoPapers = useMemo(() => (
+    Array(FIVE).fill(EMPTY).map((_, index) => (
+      <Skeleton key={`${index + TWENTY}`}>
+        <Paper sx={styles.infoPaper} />
+      </Skeleton>
+    ))
+  ), []);
+  return (
+    <>
+      <Skeleton variant="text" sx={styles.title} />
+      <Box sx={styles.container}>
+        <Skeleton sx={styles.image} variant="rectangular" />
+        <Box sx={styles.infoContainer}>
+          {infoPapers}
+          <Box sx={styles.chipContainer}>
+            {chips}
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
