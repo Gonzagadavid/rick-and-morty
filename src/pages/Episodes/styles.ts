12b304db2f37d63f4434dd6e71528fcd6a7c36ee
gonzagadavid <david.blesses@hotmail.com>
2022-08-20 23:07:41 -0300

    style: add reponsiveness in eposides page

--- src/pages/Episodes/styles.ts
+++ src/pages/Episodes/styles.ts
@@ -9,14 +9,18 @@ export const styles = {
     paddingBottom: (theme: Theme) => theme.spacing([20]),
 
   },
-  accordionText: {
-    fontSize: (theme: Theme) => theme.spacing([2]),
-    fontFamily: (theme: Theme) => theme.typography.h5,
-    margin: (theme: Theme) => theme.spacing([5]),
-    width: (theme: Theme) => theme.percent(25),
+  accordionText: (theme: Theme) => ({
+    fontSize: theme.spacing([2]),
+    fontFamily: theme.typography.h5,
+    margin: theme.spacing([5]),
+    width: theme.percent(25),
     textTransform: 'capitalize',
     wordSpacing: '1rem',
-  },
+    [theme.breakpoints.down('sm')]: {
+      fontFamily: theme.typography.h6,
+      width: theme.percent(23),
+    },
+  }),
   charContainer: {
     display: 'flex',
     alignItems: 'center',
